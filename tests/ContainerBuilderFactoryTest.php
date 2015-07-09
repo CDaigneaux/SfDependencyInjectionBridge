229bf433a8edc9a441dfc25027251c5d4fb4aa59
Cyrille Daigneaux <cyrille.daigneaux@gmail.com>
2015-07-09 16:39:05 +0200

    add container builder factory test

--- tests/ContainerBuilderFactoryTest.php
+++ tests/ContainerBuilderFactoryTest.php
@@ -13,21 +13,73 @@ class ContainerBuilderFactoryTest extends \PHPUnit_Framework_TestCase
      */
     public function createService_ShouldReturnInstanceOfContainerBuilder()
     {
-        $moduleManager = $this->getMockBuilder(ModuleManager::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $moduleManager = $this->getModuleManager();
         $moduleManager
             ->method('getLoadedModules')
             ->will($this->returnValue([]));
         
-        $serviceLocator = $this->getMock(ServiceLocatorInterface::class);
+        $serviceLocator = $this->getServiceLocator();
         $serviceLocator
             ->method('get')
             ->with('ModuleManager')
             ->will($this->returnValue($moduleManager));
 
-        $containerBuilderFactory = new ContainerBuilderFactory();
+        $this->assertInstanceOf(ContainerBuilder::class, $this->getContainerBuilder($serviceLocator));
+    }
 
-        $this->assertInstanceOf(ContainerBuilder::class, $containerBuilderFactory->createService($serviceLocator));
+    /**
+     * @test
+     */
+    public function createService_WithExistingService_ShouldGetTheServiceSetted()
+    {
+        $module = $this->getModule([
+            'sf-dependency-injection' => [
+                'service-locator' => [
+                    'ServiceTest' => 'ServiceTestInServiceLocator'
+                ]
+            ]
+        ]);
+        $moduleManager = $this->getModuleManager();
+        $moduleManager
+            ->method('getLoadedModules')
+            ->will($this->returnValue([$module]));
+
+        $serviceString  = 'This is a test service in the service locator';
+        $serviceLocator = $this->getServiceLocator();
+        $serviceLocator
+            ->method('get')
+            ->will($this->returnValueMap([
+                ['ModuleManager', $moduleManager],
+                ['ServiceTestInServiceLocator', $serviceString]
+            ]));
+
+        $this->assertSame($serviceString, $this->getContainerBuilder($serviceLocator)->get('ServiceTest'));
+    }
+
+    private function getModuleManager()
+    {
+        return $this->getMockBuilder(ModuleManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    private function getServiceLocator()
+    {
+        return $this->getMock(ServiceLocatorInterface::class);
+    }
+
+    private function getContainerBuilder(ServiceLocatorInterface $serviceLocator)
+    {
+        $containerBuilderFactory = new ContainerBuilderFactory();
+        return $containerBuilderFactory->createService($serviceLocator);
+    }
+
+    private function getModule(array $config)
+    {
+        $moduleTest = $this->getMock('Module', ['getConfig']);
+        $moduleTest
+            ->method('getConfig')
+            ->will($this->returnValue($config));
+        return $moduleTest;
     }
 }

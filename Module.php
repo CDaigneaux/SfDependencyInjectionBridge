e565b6757ed651ebc039f90295b7a332e2d0c791
Nicolas Eeckeloo <neeckeloo@gmail.com>
2015-07-09 13:49:50 +0200

    Update Module class

--- Module.php
+++ Module.php
@@ -1,18 +1,12 @@
 <?php
 namespace SfDependencyInjectionBridge;
 
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
-class Module
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
@@ -21,7 +15,7 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\StandardAutoloader' => array(
+            StandardAutoloader::class => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/',
                 ),

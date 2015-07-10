c5aa047d974f1ac2e7eb6bf1b483ab781b17605e
Cyrille Daigneaux <cyrille.daigneaux@gmail.com>
2015-07-10 14:56:32 +0200

    add missing Module config

---
+++ src/Module.php
@@ -0,0 +1,25 @@
+<?php
+namespace Cdx\SfDependencyInjectionBridge;
+
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            StandardAutoloader::class => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
+    }
+}
